2af9f124aff6b46178c15d6e4f33d424f8058151
wzd <15027730753@163.com>
2026-04-29 19:10:37 +0800

    1

--- 11 基础排序/11 基础排序/11 冒泡排序.cpp
+++ 11 基础排序/11 基础排序/11 冒泡排序.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //冒泡排序
 void BubbleSort(int arr[], int size) {
 	//排序趟数
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size - 1; i++) {
 		bool flag = false;
 		//一趟排序次数
 		for (int j = 0; j < size - 1 -i; j++) {
@@ -21,6 +21,23 @@ void BubbleSort(int arr[], int size) {
 	}
 }
 
+//选择排序 - 时间复杂度：O(n^2) - 空间复杂度：O(1) - 稳定性：不稳定
+void ChoiceSort(int arr[], int size) {
+	//排序趟数 - O(n)
+	for (int i = 0; i < size - 1; i++) {
+		//一趟排序 - O(n)
+		int min = i;
+		for (int j = i + 1; j < size; j++) {
+			if (arr[min] > arr[j])	min = j;
+		}
+		if (min != i) {
+			int tmp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = tmp;
+		}
+	}
+}
+
 int main() {
 	int arr[10];
 	srand(time(NULL));
@@ -31,7 +48,7 @@ int main() {
 	}
 	cout << endl;
 
-	BubbleSort(arr, sizeof(arr) / sizeof(arr[0]));
+	ChoiceSort(arr, sizeof(arr) / sizeof(arr[0]));
 
 	for (int i : arr) {
 		cout << i << " ";
