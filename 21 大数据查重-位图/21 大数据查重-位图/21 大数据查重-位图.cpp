47dded306184387e0b469af3518dd1100f3fcbe9
wzd <15027730753@163.com>
2026-05-18 22:42:44 +0800

    1

---
+++ 21 大数据查重-位图/21 大数据查重-位图/21 大数据查重-位图.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<int> vec = {12, 78, 90, 78, 123, 8, 9, 90};
+
+	//查找最大值
+	int max = vec[0];
+	for (auto v : vec) {
+		if (v > max) {
+			max = v;
+		}
+	}
+	//cout << max << endl;
+
+	/*
+	栈stack上数组大小必须是常量
+	   int arr[100];
+	堆heap才支持动态大小
+	   int* arr = new int[max];
+	*/
+	int *bitmap = new int[max / 32 + 1](); //位图数组全部初始化为0
+	for (auto v : vec) {
+		int idx = v / 32;
+		int ofs = v % 32;
+		//左移运算符 << 的优先级 高于 按位或运算符 | &
+		if (bitmap[idx] & 1 << ofs) {
+			cout << v << " 重复出现过!" << endl;
+			//return; //找第一次重复出现的 
+		}
+		else {
+			bitmap[idx] = bitmap[idx] | 1 << ofs;
+		}
+	}
+	delete[] bitmap;
+}
---
+++ 22 大数据查重-布隆过滤器/22 大数据查重-布隆过滤器/布隆过滤器.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include "stringhash.h"
+
+using namespace std;
+
+//布隆过滤器
+class BloomFilter {
+public:
+	BloomFilter(int bitSize = 1471) 
+		: bitSize_(bitSize)
+	{
+		bitMap_.resize(bitSize_ / 32 + 1); //位图数组初始化为0
+	}
+public:
+	//增加元素
+	void setBit(const string* str) {
+		// 计算k组哈希函数的值
+	    // 经过哈希函数处理后不一定落在位图范围内，把大数映射到 位图数组内
+		//外层函数已经拿到了指针，内层函数直接用这个指针传参即可，不需要再取地址。
+		int idx1 = BKDRHash(str) % bitSize_;
+		int idx2 = RSHash(str) % bitSize_;
+		int idx3 = APHash(str) % bitSize_;
+
+		int idx = 0;
+		int ofs = 0;
+
+		idx = idx1 / 32;
+		ofs = idx1 % 32;
+		//bitMap_[idx] = bitMap_[idx] | (1 << ofs);
+		bitMap_[idx] |= (1 << ofs);
+
+		idx = idx2 / 32;
+		ofs = idx2 % 32;
+		bitMap_[idx] |= (1 << ofs);
+
+		idx = idx3 / 32;
+		ofs = idx3 % 32;
+		bitMap_[idx] |= (1 << ofs);
+	}
+	//查找元素
+	bool getBit(const string* str) {
+		int idx1 = BKDRHash(str) % bitSize_;
+		int idx2 = RSHash(str) % bitSize_;
+		int idx3 = APHash(str) % bitSize_;
+
+		int idx = 0;
+		int ofs = 0;
+
+		idx = idx1 / 32;
+		ofs = idx1 % 32;
+		if ((bitMap_[idx] & (1 << ofs)) == 0) {
+			return false;
+		}
+
+		idx = idx2 / 32;
+		ofs = idx2 % 32;
+		if ((bitMap_[idx] & (1 << ofs)) == 0) {
+			return false;
+		}
+
+		idx = idx3 / 32;
+		ofs = idx3 % 32;
+		if ((bitMap_[idx] & (1 << ofs)) == 0) {
+			return false;
+		}
+		//如果上面都为1，元素存在
+		return true;
+	}
+private:
+	int bitSize_;  //位图大小
+	vector<int> bitMap_; //位图数组
+};
+
+//黑名单
+class BlackList {
+public:
+	void add(string url) {
+		bloomFilter_.setBit(&url);
+	}
+	bool query(string url) {
+		return bloomFilter_.getBit(&url);
+	}
+private:
+	BloomFilter bloomFilter_;
+};
+
+int main() {
+	BlackList list;
+	list.add("http://www.baidu.com");
+	list.add("http://www.360buy.com");
+	list.add("http://www.tmall.com");
+	list.add("http://www.tencent.com");
+
+	string url = "http://www.alibaba.com";
+	cout << list.query(url) << endl;
+}
