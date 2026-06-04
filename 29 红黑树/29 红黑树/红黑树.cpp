8cadb42b4934d2798705c6fa5283df584cd16004
wzd <15027730753@163.com>
2026-06-04 11:39:49 +0800

    1

--- 29 红黑树/29 红黑树/红黑树.cpp
+++ 29 红黑树/29 红黑树/红黑树.cpp
@@ -57,9 +57,40 @@ public:
 	}
 public:
 	//左旋转操作-非递归
+	//在递归操作中只需要3行代码，旋转后的根节点与父亲节点的连接是在递归回溯时完成的，非递归操作需要加更多的代码
 	void leftRotate(Node* node)
 	{
+		Node* child = node->right_;
+		node->right_ = child->left_;
 
+		if (child->left_ != nullptr)
+		{
+			child->left_->parent_ = node;
+		}
+
+		//旋转后的根节点与父节点连接
+		if (node->parent_ == nullptr)
+		{
+			root_ = child;
+		}
+		else
+		{
+			//判断node是在父节点的左或右子树
+			if (node == node->parent_->left_)
+			{
+				node->parent_->left_ = child;
+			}
+			else 
+			{
+				node->parent_->right_ = child;
+			}
+		}
+		//修改child节点的父节点
+		child->parent_ = node->parent_;
+
+		child->left_ = node;
+		//修改node节点的父节点
+		node->parent_ = child;
 	}
 	//右旋转操作-非递归
 	void leftRotate(Node* node)
